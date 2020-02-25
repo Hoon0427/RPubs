66a95ada77b9f7f449501da9c912ff13284262c2
Hoon0427 <ikaros977@gmail.com>
2020-02-25 12:31:55 +0900

    fixed_200225

--- RPubs_iris.R
+++ RPubs_iris.R
@@ -1,5 +1,3 @@
-install.packages("DT")
-install.packages("ggvis")
 
 library(kernlab)
 library(nnet)
@@ -41,3 +39,59 @@ rpart_p<-predict(rpart_m, newdata = test_set, type = "class")
 rf_m <- randomForest(Species ~ Petal.Length + Petal.Width, data=training_set)
 rf_p <- predict(rf_m, newdata= test_set, type="class")
 
+svm_m <- ksvm(Species ~ Petal.Length + Petal.Width, data = training_set)
+svm_p <- predict(svm_m, newdata = test_set)
+
+normalizer <- function(x) {
+  return_value <- (x - min(x)) / (max(x) - min(x))
+  return(return_value)
+}
+
+normal_iris <- sapply(iris[,1:4], normalizer) %>% as.data.frame()
+
+# 데이터 생성
+df <- cbind(normal_iris, "Species" = iris[,5])
+
+# training / test sampling
+training_sampling <- sort(sample(1:nrow(df), nrow(df)* 0.7))
+test_sampling <- setdiff(1:nrow(df), training_sampling)
+
+# training_set, test_set
+training_set <- df[training_sampling,]
+test_set <- df[test_sampling,]
+
+training_set_unlable <- training_set[,1:4]
+training_set_lable <- training_set[,5]
+
+test_set_unlable <- test_set[,1:4]
+test_set_lable <- test_set[,5]
+
+knn_p <- knn(train = training_set_unlable, test = test_set_unlable, cl = training_set_lable, k =3)
+
+model_list <- cbind(
+  as.character(multi_logit_p),
+  as.character(rpart_p),
+  as.character(rf_p),
+  as.character(svm_p),
+  as.character(knn_p) %>% 
+    as.data.frame()
+)
+
+#str(model_list)
+install.packages("e1071")
+
+
+total_model_accuracy <- data.frame()
+for (model in model_list[, 1:ncol(model_list)]) {
+  model_cm <- confusionMatrix(model, test_set$Species)
+  model_cm_class <- model_cm$byClass %>% as.data.frame()
+  model_accuracy <- model_cm_class$'Balanced Accuracy'
+  total_model_accuracy <- rbind(total_model_accuracy, model_accuracy)
+  
+}
+
+colnames(total_model_accuracy) <- levels(test_set$Species)
+rownames(total_model_accuracy) <- c("Logistic Regression", "Decision Tree",
+                                    "Random Forest", "Support Vector Machine","KNN")
+
+datatable(total_model_accuracy)
